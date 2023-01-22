0233fb33d03913010382dd22d767c81fdca78a8e
Ferdroid8080 <andresf@ferudinato.com>
2023-01-22 18:16:37 -0500

    added habit-list and habit-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HabitListComponent } from './habit-list/habit-list.component';
+import { HabitDetailComponent } from './habit-detail/habit-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HabitListComponent, HabitDetailComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/habit-list/habit-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-list',
+  template: `
+    <p>
+      habit-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HabitListComponent {
+
+}
