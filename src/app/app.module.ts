f446d23f471f340d57583699670f3284769d51c0
Ferdroid8080 <andresf@ferudinato.com>
2023-01-22 23:48:49 -0500

    linked components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
 import { HabitDetailComponent } from './habit-detail/habit-detail.component';
+import { HabitItemComponent } from './habit-item/habit-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HabitFormComponent } from './habit-form/habit-form.component';
 
 @NgModule({
-  declarations: [AppComponent, HabitListComponent, HabitDetailComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HabitListComponent,
+    HabitDetailComponent,
+    HabitItemComponent,
+    HabitFormComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
