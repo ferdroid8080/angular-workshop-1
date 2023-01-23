b6fa97e5c945708921fd96a58fbef9f62879f795
Ferdroid8080 <andresf@ferudinato.com>
2023-01-22 23:48:37 -0500

    add habbit-form

--- src/app/habit-list/habit-list.component.ts
+++ src/app/habit-list/habit-list.component.ts
@@ -3,13 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-habit-list',
   template: `
-    <p>
-      habit-list works!
-    </p>
+    <h2>Habits</h2>
+    <app-habit-form (addHabit)="onAddHabit($event)"></app-habit-form>
+    <ul>
+      <app-habit-item
+        *ngFor="let habit of habits"
+        [habit]="habit"
+      ></app-habit-item>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class HabitListComponent {
+  habits = [
+    {
+      id: 1,
+      title: 'Check in with parents once a week',
+    },
+    {
+      id: 2,
+      title: 'Record 2 videos per day',
+    },
+    {
+      id: 3,
+      title: 'Work on side project 5 hours/week',
+    },
+    {
+      id: 4,
+      title: 'Write for 20 minutes a day',
+    },
+  ];
 
+  onAddHabit(newHabit: any) {
+    const id = this.habits.length + 1;
+    newHabit.id = id;
+    this.habits.push(newHabit);
+  }
 }
