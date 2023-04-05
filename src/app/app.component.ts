376637c86bc4ea942a7fb46e72d4473aa290edbf
Ferdroid8080 <andresf@ferudinato.com>
2023-04-04 23:36:38 -0500

    add routing to an existing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
     <div class="container">
       <h1>{{ title }}</h1>
       <app-habit-list></app-habit-list>
+      <router-outlet></router-outlet>
     </div>
   `,
   styles: ['h1 { color: blue }'],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
@@ -8,6 +9,12 @@ import { HabitDetailComponent } from './habit-detail/habit-detail.component';
 import { HabitItemComponent } from './habit-item/habit-item.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HabitFormComponent } from './habit-form/habit-form.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -16,8 +23,14 @@ import { HabitFormComponent } from './habit-form/habit-form.component';
     HabitDetailComponent,
     HabitItemComponent,
     HabitFormComponent,
+    HomeComponent,
   ],
-  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
