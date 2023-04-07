9f5f40c9988ebe01eea3586d34a4b8332366f02d
Ferdroid8080 <andresf@ferudinato.com>
2023-04-07 10:22:41 -0500

    add parent route params

--- src/app/system-info/system-info.component.ts
+++ src/app/system-info/system-info.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { SystemService } from '../data/system.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-system-info',
   template: `
-    <p>
-      system-info works!
-    </p>
+    <h3>Information</h3>
+    <p>{{ info }}</p>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class SystemInfoComponent {
+  id!: number;
+  info!: string;
 
+  constructor(
+    private systemService: SystemService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.parent &&
+      this.route.parent.paramMap
+        .pipe(
+          switchMap((params: ParamMap) => {
+            this.id = +params.get('id')!;
+            return this.systemService.getSystemInfo(this.id);
+          })
+        )
+        .subscribe((data) => {
+          this.info = data;
+        });
+  }
 }
--- src/app/system-items/system-items.component.ts
+++ src/app/system-items/system-items.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
+import { Item } from '../data/system';
+import { SystemService } from '../data/system.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-system-items',
   template: `
-    <p>
-      system-items works!
-    </p>
+    <h3>Example Gear</h3>
+    <ul>
+      <li *ngFor="let item of items">
+        {{ item.name }} - {{ item.description }}
+      </li>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class SystemItemsComponent {
+  items: Item[] = [];
+  id!: number;
 
+  constructor(
+    private systemService: SystemService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.parent &&
+      this.route.parent.paramMap
+        .pipe(
+          switchMap((params: ParamMap) => {
+            this.id = +params.get('id')!;
+            return this.systemService.getSystemItems(this.id);
+          })
+        )
+        .subscribe((data) => {
+          this.items = data;
+        });
+  }
 }
